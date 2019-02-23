3a516c467f4dc7ddf965a9179ee49517d951cd88
Tushar Kapila <tgkprog@gmail.com>
2019-02-24 01:41:43 +0530

    Queue with underflow check and returns remvoed element on function remove()

--- app.js
+++ app.js
@@ -7,14 +7,17 @@ Queue.prototype.add = function(record) {
 }
 
 Queue.prototype.remove = function() {
-  this.data.pop();
+  if(this.data.length < 1)return "Empty queue";//or throw new Exception ("Queue empty");	
+  return this.data.pop();
 }
 
 Queue.prototype.first = function() {
+  if(this.data.length < 1)return "Empty queue";//or throw new Exception ("Queue empty");	 	
   return this.data[0];
 }
 
 Queue.prototype.last = function() {
+  if(this.data.length < 1)return "Empty queue";//or throw new Exception ("Queue empty");	 	
   return this.data[this.data.length - 1];
 }
 
@@ -27,6 +30,15 @@ const q = new Queue();
 q.add(1);
 q.add(2);
 q.add(3);
-q.remove();
+var i =0;
+console.log(`${++i} remove `+ q.remove());
 console.log(q);
+console.log(`Size of the Queue: ${q.size()}`)
+
+console.log(`${++i} remove `+ q.remove());
+console.log(`${++i} remove `+ q.remove());
+console.log(`${++i} remove `+ q.remove());
+console.log(`${++i} remove `+ q.remove());
+console.log(`${++i} remove `+ q.remove());
+
 console.log(`Size of the Queue: ${q.size()}`)
